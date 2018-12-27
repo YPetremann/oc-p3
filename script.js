1de751dfb222761410a419dbd78e01de7142c542
Y.Petremann <y.petremann@gmail.com>
2018-12-27 17:42:05 +0100

    Upgrading script to better style

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 var apiKey = "2ac5382f413686e3d4abf62a926b20ff88b74dd5"
 var baseUrl = "https://api.jcdecaux.com/vls/v1"
 
-var contract = {
-	name: "Lyon"
-}
+var contract = {}
 
 var els = {}
 
@@ -57,6 +55,7 @@ function signMouseMove(e) {
 		els.ctx.stroke();
 	}
 }
+
 function clusterIcon(cluster) {
 	let bike_available = 0
 	let station_available = cluster.getChildCount()
@@ -64,18 +63,96 @@ function clusterIcon(cluster) {
 		bike_available += station.available
 	}
 	let average = bike_available / station_available
-	console.log(cluster)
 	return L.divIcon({
 		iconSize: [25, 25],
-		className: average < 2 ? "group0" :
-				 average < 4 ? "group25" :
-				 average < 6 ? "group50" :
-				 average < 8 ? "group75" :
-				 "group100",
+		className: average < 2 ? "group0" : average < 4 ? "group25" : average < 6 ? "group50" : average < 8 ? "group75" : "group100",
 		html: `<b>${bike_available}</b>`
 	});
 	return
 }
+
+async function reloadmap(city) {
+	//try {
+	// get information about contracts
+	contract.name = city
+	var contracts = JSON.parse(await ajaxGET(baseUrl + "/contracts?&apiKey=" + apiKey))
+	els.cities.innerHTML = ""
+	// complete current contract from contract name
+	for (tmpContract of contracts) {
+		els.cities.appendChild(_(`option[value="${tmpContract.name}"]`, `${tmpContract.name} : ${tmpContract.commercial_name}`))
+		if (tmpContract.name == contract.name) {
+			els.cities.value = tmpContract.name
+			contract = tmpContract
+		}
+	}
+
+	// complete presentation from contract infos
+	els.welcome_title.textContent = `${contract.commercial_name}`
+	els.welcome_sentence.textContent = `Bienvenu sur le site de réservation de vélo pour ${contract.name}`
+
+	// get station list from contract name
+	var stations = JSON.parse(await ajaxGET(baseUrl + "/stations?contract=" + contract.name + "&apiKey=" + apiKey))
+
+	// initialise map
+	oldmap = els.map
+	oldmap.parentNode.replaceChild(els.map = _('#mapid'), oldmap)
+	var mymap = L.map(els.map, {
+		maxBoundsViscosity: 0.5,
+		bounceAtZoomLimits: false,
+		zoomSnap: 0,
+		scrollWheelZoom: false,
+		maxZoom: 18
+	});
+	coords = [
+		[Infinity, Infinity],
+		[-Infinity, -Infinity]
+	]
+
+	var markerscluster = L.markerClusterGroup({
+		iconCreateFunction: clusterIcon
+	})
+	for (station of stations) {
+		coords[0][0] = Math.min(coords[0][0], station.position.lat)
+		coords[0][1] = Math.min(coords[0][1], station.position.lng)
+		coords[1][0] = Math.max(coords[1][0], station.position.lat)
+		coords[1][1] = Math.max(coords[1][1], station.position.lng)
+
+		let marker = L.marker([station.position.lat, station.position.lng])
+
+		marker.available = station.available_bikes
+		marker.options.icon = L.divIcon({
+			iconSize: [25, 25],
+			className: marker.available < 2 ? "station0" : marker.available < 4 ? "station25" : marker.available < 6 ? "station50" : marker.available < 8 ? "station75" : "station100",
+			html: `<b>${marker.available}</b>`
+		});
+		marker.options.riseOnHover = true
+		marker.number = station.number
+		marker.on({
+			click: highlightStation
+		})
+		markers.push(sel.substring(1))
+		markerscluster.addLayer(marker)
+		//marker.addTo(mymap)
+	}
+
+	coords[0][0] = coords[0][0] - (coords[1][0] - coords[0][0]) * 0.1
+	coords[0][1] = coords[0][1] - (coords[1][1] - coords[0][1]) * 0.1
+	coords[1][0] = coords[1][0] + (coords[1][0] - coords[0][0]) * 0.1
+	coords[1][1] = coords[1][1] + (coords[1][1] - coords[0][1]) * 0.1
+
+	mymap.options.minZoom = mymap.getBoundsZoom(coords, false) - 0.5
+	mymap.setMaxBounds(coords)
+	mymap.fitBounds(coords)
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(mymap);
+	markerscluster.addTo(mymap)
+	/*
+	} catch (err) {
+		els.welcome_title.textContent = `Erreur`
+		els.welcome_sentence.textContent = `${err}`
+	}
+	*/
+}
+async function reloadbook() {}
 var markers = []
 var error = []
 async function main() {
@@ -89,7 +166,9 @@ async function main() {
 			},
 			_('section#carousel.page',
 				_('header.multibar',
-					_('h2.left', 'Présentation')
+					_('h2.left', 'Présentation'),
+					_('a.right.fas.fa-map-marked-alt[href="#map"]'),
+					_('a.right.fas.fa-ticket-alt[href="#booking"]')
 				),
 				_('.carousel',
 					_('.slides',
@@ -99,7 +178,8 @@ async function main() {
 						),
 						_('#slide2.slide',
 							_('h3', 'Trouvez votre vélo'),
-							_('p', 'Aidez-vous de la carte afin de trouver la station de vélos la plus proche, cliquez sur le marqueur pour voir les details')
+							_('p', 'Aidez-vous de la carte afin de trouver la station de vélos la plus proche, cliquez sur le marqueur pour voir les details'),
+							_('a[href="#map"]', 'Voir la Carte')
 						),
 						_('#slide3.slide',
 							_('h3', 'Réservez le !'),
@@ -107,7 +187,8 @@ async function main() {
 						),
 						_('#slide4.slide',
 							_('h3', 'Vérifiez votre réservation'),
-							_('p', 'Votre reservation est effectué, vous pouvez à tout moment voir le détail de vos reservations, (une seule reservation est acceptée pour le moment)')
+							_('p', 'Votre reservation est effectué, vous pouvez à tout moment voir le détail de vos reservations, (une seule reservation est acceptée pour le moment)'),
+							_('a[href="#booking"]', 'Voir vos réservations')
 						)
 					),
 					_('ul.navigation',
@@ -122,26 +203,34 @@ async function main() {
 					)
 				)
 			),
-			_('section#map.page',{events: {click: function() {els.details.classList.remove("open")}}},
+			_('section#map.page', {
+					events: {
+						click: function() {
+							els.details.classList.remove("open")
+						}
+					}
+				},
 				_('header.multibar',
 					_('h2.left', 'Carte'),
 					els.cities = _('select.right', {
 						events: {
 							change: function() {
-								reload(els.cities.value)
+								reloadmap(els.cities.value)
 							}
 						}
-					}, _('option[value]', `(Ville)`))
+					}, _('option[value]', `(Ville)`)),
+					_('a.right.fas.fa-ticket-alt[href="#booking"]'),
+					_('a.right.fas.fa-book[href="#carousel"]')
 				),
 				els.map = _('#mapid'),
 			),
 			els.details = _('section#details.page',
 				_('header', _('h2.multibar',
 					_('.left', 'Details'),
-					_('i.right.fas.fa-times', {
+					_('a.right.fas.fa-times', {
 						events: {
 							click: function() {
-								els.details.classList.toggle("open")
+								els.details.classList.remove("open")
 							}
 						}
 					})
@@ -150,10 +239,15 @@ async function main() {
 				_('p', 'Adresse : ', els.detail_address = _("span", "...")),
 				_('p', els.detail_size = _("span", "..."), " places"),
 				_('p', els.detail_current = _("span", "..."), " vélos disponibles"),
-				els.detail_form = _('form',
-					els.detail_number = _('input[name="id" type="hidden"]'),
-					_("p", _('label[for="name"]', 'Nom :'), _('input#name[name="name" type="text"]')),
-					_("p", _('label[for="surname"]', 'Prénom :'), _('input#surname[name="surname" type="text"]')),
+				els.detail_form = _('form', {
+						events: {
+							submit: bookSubmit
+						}
+					},
+					els.detail_city = _('input[name="city" type="hidden"]'),
+					els.detail_number = _('input[name="number" type="hidden"]'),
+					_("p", _('label[for="name"]', 'Nom :'), els.name = _('input#name[name="name" type="text"]')),
+					_("p", _('label[for="surname"]', 'Prénom :'), els.surname = _('input#surname[name="surname" type="text"]')),
 					_('input[name="sign" type="hidden"]'),
 					_("p", _('label[for="sign"]', 'Signature :'), els.sign = _('canvas#sign', {
 						events: {
@@ -164,95 +258,19 @@ async function main() {
 					_('input[value="Réserver" type="submit"]')
 				)
 			),
-			_('section#booking.page',
-				_('header', _('h2.hidden', 'Reservations'))
+			els.bookings = _('section#booking.page',
+				_('header.multibar',
+					_('h2.hidden', 'Reservations'),
+					_('a.right.fas.fa-book[href="#carousel"]'),
+					_('a.right.fas.fa-map-marked-alt[href="#map"]')
+				)
 			)
 		)
 	)
-	async function reload(city){
-		//try {
-			// get information about contracts
-			contract.name = city
-			var contracts = JSON.parse(await ajaxGET(baseUrl + "/contracts?&apiKey=" + apiKey))
-			els.cities.innerHTML = ""
-			// complete current contract from contract name
-			for (tmpContract of contracts) {
-				els.cities.appendChild(_(`option[value="${tmpContract.name}"]`, `${tmpContract.name} : ${tmpContract.commercial_name}`))
-				if (tmpContract.name == contract.name) {
-					els.cities.value = tmpContract.name
-					contract = tmpContract
-				}
-			}
+	if (localStorage.getItem("name")) els.name.value = localStorage.getItem("name");
+	if (localStorage.getItem("surname")) els.surname.value = localStorage.getItem("surname");
+	await reloadmap("Lyon")
 
-			// complete presentation from contract infos
-			els.welcome_title.textContent = `${contract.commercial_name}`
-			els.welcome_sentence.textContent = `Bienvenu sur le site de réservation de vélo pour ${contract.name}`
-
-			// get station list from contract name
-			var stations = JSON.parse(await ajaxGET(baseUrl + "/stations?contract=" + contract.name + "&apiKey=" + apiKey))
-
-			// initialise map
-			oldmap = els.map
-			oldmap.parentNode.replaceChild(els.map = _('#mapid'), oldmap)
-			var mymap = L.map(els.map, {
-				maxBoundsViscosity: 0.5,
-				bounceAtZoomLimits: false,
-				zoomSnap: 0,
-				scrollWheelZoom: false,
-				maxZoom: 18
-			});
-			coords = [
-				[Infinity, Infinity],
-				[-Infinity, -Infinity]
-			]
-
-			var markerscluster = L.markerClusterGroup({iconCreateFunction: clusterIcon})
-			for (station of stations) {
-				coords[0][0] = Math.min(coords[0][0], station.position.lat)
-				coords[0][1] = Math.min(coords[0][1], station.position.lng)
-				coords[1][0] = Math.max(coords[1][0], station.position.lat)
-				coords[1][1] = Math.max(coords[1][1], station.position.lng)
-
-				let marker = L.marker([station.position.lat, station.position.lng])
-
-				marker.available = station.available_bikes
-				marker.options.icon = L.divIcon({
-					iconSize: [25, 25],
-					className: marker.available < 2 ? "station0" :
-							 marker.available < 4 ? "station25" :
-							 marker.available < 6 ? "station50" :
-							 marker.available < 8 ? "station75" :
-							 "station100",
-					html: `<b>${marker.available}</b>`
-				});
-				marker.options.riseOnHover = true
-				marker.number = station.number
-				marker.on({
-					click: highlightStation
-				})
-				markers.push(sel.substring(1))
-				markerscluster.addLayer(marker)
-				//marker.addTo(mymap)
-			}
-
-			coords[0][0] = coords[0][0] - (coords[1][0] - coords[0][0]) * 0.1
-			coords[0][1] = coords[0][1] - (coords[1][1] - coords[0][1]) * 0.1
-			coords[1][0] = coords[1][0] + (coords[1][0] - coords[0][0]) * 0.1
-			coords[1][1] = coords[1][1] + (coords[1][1] - coords[0][1]) * 0.1
-
-			mymap.options.minZoom = mymap.getBoundsZoom(coords, false) - 0.5
-			mymap.setMaxBounds(coords)
-			mymap.fitBounds(coords)
-			L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(mymap);
-			markerscluster.addTo(mymap)
-			/*
-		} catch (err) {
-			els.welcome_title.textContent = `Erreur`
-			els.welcome_sentence.textContent = `${err}`
-		}
-		*/
-	}
-	await reload("Lyon")
 	var anchoreds = document.querySelectorAll("a[href*='#']:not([href='#'])");
 	for (anchored of anchoreds) {
 		anchored.addEventListener("click", function(e) {
