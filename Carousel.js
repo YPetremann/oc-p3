533741bd86e671140af7ef74725623b39a9f46c3
Y.Petremann <y.petremann@gmail.com>
2018-12-28 15:05:53 +0100

    Cleanup Carousel

--- Carousel.js
+++ Carousel.js
@@ -4,29 +4,27 @@ Number.prototype.mod = function(n) {
 
 class Carousel {
 	constructor(element) {
-		// bind methods usable for events
-		this.jump = this.jump.bind(this)
-		this.prev = this.prev.bind(this)
-		this.next = this.next.bind(this)
-		this.play = this.play.bind(this)
-		this.pause = this.pause.bind(this)
-		//this._ = this._.bind(this)
-
 		this.DOMelement = element
+		// bind methods usable for events to prevent object dissociation
+		for (let listener of ["jump", "prev", "next", "play", "pause"])
+			this[listener] = this[listener].bind(this)
+		// capture DOMelement instance in object
+
 		this._prev = this.DOMelement.querySelector(".navigation .prev")
 		this._next = this.DOMelement.querySelector(".navigation .next")
 		this._play = this.DOMelement.querySelector(".navigation .play")
 		this._pause = this.DOMelement.querySelector(".navigation .pause")
 
+		// attach event to DOMelements
 		for (let pager of this.pagers) pager.addEventListener("click", this.jump)
-
 		this._prev.addEventListener("click", this.prev)
 		this._next.addEventListener("click", this.next)
 		this._play.addEventListener("click", this.play)
 		this._pause.addEventListener("click", this.pause)
 
+		// Start carousel
+		this.refresh()
 		this.play()
-		this.page = this.page
 	}
 	get slides() {
 		return Array.from(this.DOMelement.querySelectorAll(".slides>*").values())
@@ -38,27 +36,31 @@ class Carousel {
 		return this.slides.length
 	}
 	get page() {
-		let current = this.DOMelement.querySelector(".slides .active") ||
+		return this.slides.indexOf(
+			this.DOMelement.querySelector(".slides .active") ||
 			this.DOMelement.querySelector(".slides *")
-		let _page = this.slides.indexOf(current)
-		return _page
+		)
 	}
 	set page(index) {
+		this.refresh(index)
+	}
+	refresh(index = null) {
+		if (index === null) index = this.page
 		index = index.mod(this.length)
-		this._page = index
-
-		// set active page
 		for (let slide of this.slides) slide.classList.remove("active")
 		for (let pager of this.pagers) pager.classList.remove("active")
 		this.slides[index].classList.add("active")
 		this.pagers[index].classList.add("active")
+		this.resettime()
+	}
+	jump(e) {
+		this.page = this.pagers.indexOf(e.target)
+	}
+	prev() {
+		this.page--
 	}
 	next() {
 		this.page++
-	}
-
-	prev() {
-		this.page--
 	}
 	play() {
 		this._play.classList.add("hidden")
@@ -69,14 +71,12 @@ class Carousel {
 		this._pause.classList.add("hidden")
 		this._play.classList.remove("hidden")
 		clearInterval(this._interval)
+		this._interval = null
 	}
 	resettime() {
-		clearInterval(this._interval)
-		this._interval = setInterval(this.next, 5000)
+		if (this._interval) {
+			clearInterval(this._interval)
+			this._interval = setInterval(this.next, 5000)
+		}
 	}
-	jump(e) {
-		this.page = this.pagers.indexOf(e.target)
-		this.resettime()
-	}
-
 }
